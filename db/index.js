f975333d070cc0ef492da48d2227e188d3d5da77
Graig <graig.mantle@gmail.com>
2023-03-27 13:18:08 -0700

    seeding and creating all tables

--- db/index.js
+++ db/index.js
@@ -9,29 +9,155 @@ const client = new Client({
 
 async function createUser({ name, email, entry }) {
     try {
-        const { rows } = await client.query(`
+        const { rows: [user] } = await client.query(`
             INSERT INTO users(name, email, entry) 
             VALUES ($1, $2, $3)
             ON CONFLICT (name) DO NOTHING;
         `, [ name, email, entry ]);
 
-        return rows
+        return user
     } catch (error) {
         throw error;
     }
 }
 
+async function updateUser(id, fields = {}) {
+
+    const setString = Object.keys(fields).map(
+        (key, index) => `"${ key }" =$${ index + 1}`
+    ).join(', ');
+
+    if (setString.length === 0) {
+        return;
+    }
+
+    try {
+        const { rows: [ user ] } = await client.query(`
+        UPDATE users
+        SET ${setString}
+        WHERE id=${id}
+        RETURNING *;
+        `, Object.values(fields));
+
+        return user;
+    } catch (error) {
+        throw error
+    }
+}
+
 async function getAllUsers() {
+    try {
     const { rows } = await client.query(
-        `SELECT id, name 
+        `SELECT * 
         FROM users
         `);
 
     return rows;
+} catch (error) {
+    throw error;
+    }
 }
+
+async function getUserById(userId) {
+    try {
+        const { rows: [ user ] } = await client.query(`
+            SELECT id, name, email, entry
+            FROM users 
+            WHERE id=${ userId };
+            `);
+        if (!user) {
+            return null
+        }
+
+        user.entries = await getEntriesByUser(userId);
+
+        return user
+    } catch (error) {
+        throw error
+    }
+}
+
+async function createEntry({
+    entryId,
+    poolaone,
+    poolbone,
+    poolbtwo,
+    poolcone,
+    poolctwo,
+    poolcthree
+}) {
+    try {
+        const { rows: [entry]} = await client.query(`
+        INSERT INTO entries("entryId", poolaone, poolbone, poolbtwo, poolcone, poolctwo, poolcthree)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        `, [entryId, poolaone, poolbone, poolbtwo, poolcone, poolctwo, poolcthree]);
+
+        return entry
+    } catch (error) {
+        throw error
+    }
+}
+
+async function updateEntry(id, fields = {}) {
+
+    const setString = Object.keys(fields).map(
+        (key, index) => `"${ key }" =$${ index + 1}`
+    ).join(', ');
+
+    if (setString.length === 0) {
+        return;
+    }
+
+    try {
+        const { rows: [ entry ] } = await client.query(`
+        UPDATE entries
+        SET ${setString}
+        WHERE id=${id}
+        RETURNING *;
+        `, Object.values(fields));
+
+        return entry;
+    } catch (error) {
+        throw error
+    }
+}
+
+async function getAllEntries() {
+    try {
+        const { rows } = await client.query(
+        `SELECT * 
+        FROM entries
+        `);
+
+    return rows;
+    
+        } catch (error) {
+            throw error
+        }
+}
+
+async function getEntriesByUser(userId) {
+    try {
+        const {rows} = await client.query( `
+            SELECT * FROM entries
+            WHERE "entryId"=${userId};
+            `);
+        
+        return rows
+    } catch (error) {
+        throw error
+    }
+}
+
 
 module.exports = {
     client,
     getAllUsers,
     createUser,
+    updateUser,
+    createEntry,
+    updateEntry,
+    getAllEntries,
+    getEntriesByUser,
+    getUserById
 }
--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,13 @@
 const { 
     client,
     getAllUsers,
-    createUser
+    createUser,
+    updateUser,
+    createEntry,
+    updateEntry,
+    getAllEntries,
+    getEntriesByUser,
+    getUserById
 } = require('./index');
 
 async function dropTables() {
@@ -9,7 +15,10 @@ async function dropTables() {
         console.log("Starting to drop tables...");
 
        await client.query(`
-            DROP TABLE IF EXISTS users;
+       DROP TABLE IF EXISTS totals;
+       DROP TABLE IF EXISTS players;
+       DROP TABLE IF EXISTS entries;
+       DROP TABLE IF EXISTS users;
        `);
 
        console.log('Finished dropping tables!')
@@ -29,8 +38,36 @@ async function createTables() {
             id SERIAL PRIMARY KEY,
             name varchar(255) UNIQUE NOT NULL,
             email varchar(255) NOT NULL,
-            entry varchar(255) NOT NULL
+            entry varchar(255) NOT NULL,
+            paid BOOLEAN DEFAULT false
         );
+        CREATE TABLE entries (
+            id SERIAL PRIMARY KEY,
+            "entryId" INTEGER REFERENCES users(id),
+            poolaone varchar(255),
+            poolbone varchar(255),
+            poolbtwo varchar(255),
+            poolcone varchar(255),
+            poolctwo varchar(255),
+            poolcthree varchar(255) 
+        );
+        CREATE TABLE players (
+            id SERIAL PRIMARY KEY,
+            mlb_id INTEGER UNIQUE,
+            fg_id INTEGER UNIQUE,
+            first_name varchar(255),
+            last_name varchar(255),
+            prev_yr INTEGER,
+            this_pred INTEGER,
+            team varchar(255),
+            groupcode varchar(255)
+        );
+        CREATE TABLE totals (
+            id SERIAL PRIMARY KEY,
+            "entryId" INTEGER REFERENCES entries(id),
+            "playerId" INTEGER REFERENCES players(id)
+
+        )
        `);
        console.log('Finished building tables!')
 
@@ -40,17 +77,57 @@ async function createTables() {
     }
 }
 
+
 async function createInitialUsers() {
     try {
         console.log("Starting to create users....");
 
-        const graig = await createUser({ name: 'Graig Mantle', email: 'graig.mantle@gmail.com', entry: 'Captain Dinger'});
-
-        console.log(graig);
+        await createUser({ name: 'Graig Mantle', email: 'graig.mantle@gmail.com', entry: 'Captain Dinger'});
+        await createUser({ name: 'Brandon Brown', email: 'brandon.brown22@gmail.com', entry: 'Commissioner'});
+        await createUser({ name: 'Greg Carathimas', email: 'greg.carathimas@gmail.com', entry: 'Greek Greg'});
 
         console.log("Finished creating users!");
     } catch(error) {
         console.error("Error creating users!");
+        throw error;
+    }
+}
+
+async function createInitialEntries() {
+    try {
+        const [graig, brandon, greg] = await getAllUsers();
+
+        await createEntry({ 
+            entryId: graig.id, 
+            poolaone: 'Aaron Judge', 
+            poolbone: 'Hunter Renfroe', 
+            poolbtwo: 'Giancarlo Stanton', 
+            poolcone: 'Jake Cronenworth',
+            poolctwo: 'Mike Yazstremski',
+            poolcthree: 'Joey Votto'
+        });
+        await createEntry({ 
+            entryId: brandon.id, 
+            poolaone: 'Mike Trout', 
+            poolbone: 'Christian Yelich', 
+            poolbtwo: 'Mitch Haniger', 
+            poolcone: 'Javier Baez',
+            poolctwo: 'Spencer Torkelson',
+            poolcthree: 'Adam Duvall'
+        });
+        await createEntry({ 
+            entryId: greg.id, 
+            poolaone: 'Vladimir Guerrero', 
+            poolbone: 'Shohei Ohtani', 
+            poolbtwo: 'Seth Brown', 
+            poolcone: 'DJ Lemahieu',
+            poolctwo: 'Gleyber Torres',
+            poolcthree: 'Rafael Devers'
+        });
+
+        console.log("Finished creating entries!");
+    } catch(error) {
+        console.error("Error creating entries!");
         throw error;
     }
 }
@@ -62,6 +139,7 @@ async function rebuildDB() {
         await dropTables();
         await createTables();
         await createInitialUsers();
+        await createInitialEntries();
     } catch (error) {
         console.error(error);
     } 
@@ -71,10 +149,34 @@ async function testDB() {
     try {
         console.log("Starting to test database...");
 
+        console.log('Calling getAllUsers');
         const users = await getAllUsers();
-        console.log("getAllUsers:", users);
+        console.log("Result:", users);
+
+        console.log('Calling getAllEntries');
+        const entries = await getAllEntries();
+        console.log("Result:", entries);
+
+        console.log('Calling update on users[0]');
+        const updateUserResult = await updateUser(users[0].id, {
+            entry: 'Home Run Homer',
+            paid: true
+        });
+        console.log('Result:', updateUserResult);
+
+        console.log('Calling update on entries[0]');
+        const updateEntryResult = await updateEntry(entries[0].id, {
+            poolaone: 'Kyle Schwarber',
+            poolcthree: 'Fernando Tatis'
+        });
+        console.log('Result:', updateEntryResult);
+
+        console.log("Calling getUserById with 2");
+        const graig = await getUserById(2);
+        console.log("Result:", graig);
 
         console.log("Finished tests!")
+
     } catch (error) {
         console.error('Error testing the database!');
         throw error
